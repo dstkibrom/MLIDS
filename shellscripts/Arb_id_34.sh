0e6bf514ad01ce646909c7af096416f3c1d020f1
dstkibbrom <dstkibrom@gmail.com>
2021-04-08 23:50:32 +0900

    shell and init_file changes

--- shellscripts/Arb_id_34.sh
+++ shellscripts/Arb_id_34.sh
@@ -3,6 +3,6 @@
 #$ -cwd
 #$ -V
 export PATH="/work/araya-kd/anaconda3/bin:$PATH"
-source activate tf_gpu
+source activate tf_gpu_cuda8
 cd /work/araya-kd/MLIDS/train_arbids
 python3 train_arbid_34.py
